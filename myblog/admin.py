ce2406f3cdbe13087d372e8b90df1547138ba874
kmsnyde <kmsnyder2@verizon.net>
2018-10-16 16:07:08 -0400

    lesson07 - assignment02

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
 from myblog.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+#@admin.register(Post, Category)
+#class Post(admin.ModelAdmin):
+#    pass
+##    fieldsets = ((None, {
+##            'fields': ('title', 'text', 'author', 'published_date')
+##            }),
+##            ('Advanced options', {
+##                    'classes': ('collapse',),
+##                    'fields': ('name', 'description', 'posts'),
+##            }),
+##    )
+
+
+
+class PostCatInline(admin.TabularInline):
+    model = Category.posts.through
+    
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'author', 'published_date')
+    inlines = [PostCatInline,]
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines =[PostCatInline,]
+    exclude = ('posts',)
+    
+
+#admin.site.register(Post)
+#admin.site.register(Category)
+
+#admin.site.register(Post)
+#
+#class PostInline(admin.TabularInline):
+#    model = Category.posts.through
+#    
+#@admin.register(Category)
+#class CategoryAdmin(admin.ModelAdmin):
+#    inlines = (PostInline,)
+#    exclude = ('posts',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+#admin.site.register(PairPostCategory)
--- myblog/models.py
+++ myblog/models.py
@@ -24,3 +24,7 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+#class PairPostCategory(models.Model):
+#    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#    category = models.ForeignKey(Category, on_delete=models.CASCADE)
